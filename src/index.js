5033c7f448d724e53ce571dde958c80fe377be9a
Joel Brewster <hi@joelbrewster.com>
2019-06-25 21:59:26 +1000

    Draw character

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("keydown", ev => console.log(ev.key));
 const canvas = document.querySelector("#game");
 
 import Font from "overprint/overprint/font";
+import Cell from "overprint/overprint/cell";
 import TextGrid from "overprint/overprint/text-grid";
 
 const grid = new TextGrid(canvas, {
@@ -11,5 +12,7 @@ const grid = new TextGrid(canvas, {
     height: 50,
     font: Font("Menlo", false, 15)
 });
+
+grid.writeCell(40, 25, Cell("@"));
 
 grid.render();
