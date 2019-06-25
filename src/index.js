f410dfab9e058f5488dfd7e0596dd9bef3d232d3
Joel Brewster <hi@joelbrewster.com>
2019-06-25 21:56:53 +1000

    Set dimensions and font

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ document.addEventListener("keydown", ev => console.log(ev.key));
 
 const canvas = document.querySelector("#game");
 
+import Font from "overprint/overprint/font";
 import TextGrid from "overprint/overprint/text-grid";
 
-const grid = new TextGrid(canvas);
+const grid = new TextGrid(canvas, {
+    width: 80,
+    height: 50,
+    font: Font("Menlo", false, 15)
+});
 
 grid.render();
