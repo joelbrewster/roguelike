e81feb6e9845d12c479dafa31291a43f5dad1980
Joel Brewster <hi@joelbrewster.com>
2019-06-25 21:55:14 +1000

    Draw canvas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // Log keypress events to the console
 document.addEventListener("keydown", ev => console.log(ev.key));
+
+const canvas = document.querySelector("#game");
+
+import TextGrid from "overprint/overprint/text-grid";
+
+const grid = new TextGrid(canvas);
+
+grid.render();
