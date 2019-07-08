3b61dc9c4be5bca04f194a26964f9f40496eec67
Joel Brewster <hi@joelbrewster.com>
2019-07-08 20:36:46 +1000

    Wall scaffolding

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,20 +1,38 @@
 /*jshint esversion: 6 */
 
 import Tiles from './tiles';
+import rung from "rung/src/rung";
 
 function generateDungeon(width, height) {
-  const tiles = [];
+    const tiles = [];
 
-  for (let y=0; y < height; y++) {
-    tiles[y] = [];
-    for (let x=0; x < width; x++) {
-      tiles[y][x] = Tiles.WALL;
+    for (let y=0; y < height; y++) {
+        tiles[y] = [];
+        for (let x=0; x < width; x++) {
+            tiles[y][x] = Tiles.WALL;
+        }
     }
-  }
 
-  return tiles
+    return tiles
+}
+
+class Rect {
+    constructor(x, y, width, height) {
+        this.x1 = x;
+        this.y1 = y;
+        this.x2 = x + width;
+        this.y2 = y + height;
+    }
+
+    forEach(callback) {
+        for (let y=this.y1+1; y < this.y2-1; y++) {
+            for (let x=this.x1+1; x < this.x2-1; x++) {
+                callback(x, y);
+            }
+        }
+    }
 }
 
 export {
-  generateDungeon
+    generateDungeon
 }
--- src/stage.js
+++ src/stage.js
@@ -1,34 +1,18 @@
 /*jshint esversion: 6 */
 
 import Tiles from "./tiles";
+import { generateDungeon } from "./dungeon";
 
 class Stage {
     constructor(width, height, player) {
         this.width = width;
         this.height = height;
-        this.map = this.initializeMap();
+        this.map = generateDungeon(width, height);
         this.player = player;
     }
 
     canMoveTo(x, y) {
         return !this.map[y][x].blocking;
-    }
-
-    initializeMap() {
-        const tiles = [];
-
-        for (let y=0; y < this.height; y++) {
-            tiles[y] = [];
-            for (let x=0; x < this.width; x++) {
-                tiles[y][x] = Tiles.FLOOR;
-            }
-        }
-
-        tiles[30][22] = Tiles.WALL;
-        tiles[31][22] = Tiles.WALL;
-        tiles[32][22] = Tiles.WALL;
-
-        return tiles;
     }
 
     renderMap(stage) {
